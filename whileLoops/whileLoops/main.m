78440acf34ccd9ed14884d49185dd57c3ea13e9e
Shena Yoshida <sheyoshida@gmail.com>
2015-06-13 10:56:41 -0400

    first

---
+++ whileLoops/whileLoops/main.m
@@ -0,0 +1,29 @@
+//
+//  main.m
+//  whileLoops
+//
+//  Created by Shena Yoshida on 2015-06-13.
+//  Copyright (c) 2015 Shena Yoshida. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    
+    // 1st exercise (it works!)
+//    int count = 10;
+//    while (count > 0) {
+//        printf ("%d \n", count);
+//        count = count - 1;
+
+    
+    //2nd exercise:
+    int count = 100;
+    while (count > 10) {
+        printf ("%d \n", count);
+        count = count - 5;
+    }
+    
+    
+    return 0;
+}
